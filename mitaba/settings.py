7cd70b7e2ba851812193b36a56e74ac9871f8a1d
slava <horlet@gmail.com>
2017-07-07 02:24:00 +0300

    environment variables settings for docker, ability to run outside of container with sqlite db

--- mitaba/settings.py
+++ mitaba/settings.py
@@ -75,14 +75,10 @@ WSGI_APPLICATION = 'mitaba.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -121,3 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if os.environ.get('DJ_ENV') == 'production':
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    DEBUG = False
+    DATABASES['default'].update({
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
+    })
+    ALLOWED_HOSTS = os.environ.get('DJ_ENV').split(',')
