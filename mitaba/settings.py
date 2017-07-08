b95b19ed239cac5a5431a8b299095612947ab226
slava <horlet@gmail.com>
2017-07-08 23:49:20 +0300

    application urls, settings

--- mitaba/settings.py
+++ mitaba/settings.py
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'mitaba.tracker.apps.TrackerConfig'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -117,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Environment dependent settings
 
 if os.environ.get('DJ_ENV') == 'production':
     SECRET_KEY = os.environ.get('SECRET_KEY')
