18a9265d39da0ad5e4f3a42cce2895f7e1d26e90
slava <horlet@gmail.com>
2017-07-08 23:49:36 +0300

    Entry model

--- mitaba/tracker/models.py
+++ mitaba/tracker/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+# from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Entry(models.Model):
+    start = models.DateTimeField()
+    stop = models.DateTimeField()
+    # details = ArrayField() todo: convert to ArrayField lately
+    details = models.TextField()
