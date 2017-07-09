335df0ea1793c6f86bcff18b7751caecc8049198
slava <horlet@gmail.com>
2017-07-09 15:53:07 +0300

    docker - local database volume link gitignore - added db folder

--- mitaba/tracker/models.py
+++ mitaba/tracker/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-# from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.fields import ArrayField
 
 
 class Entry(models.Model):
     start = models.DateTimeField()
     stop = models.DateTimeField()
-    # details = ArrayField() todo: convert to ArrayField lately
-    details = models.TextField()
+    details = ArrayField(
+        models.CharField(max_length=500, blank=False),
+        blank=False,
+        max_length=20
+    )
