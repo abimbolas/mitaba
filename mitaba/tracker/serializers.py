6ddd2697c77a067e17ca6f3105d3a98dfc68859e
slava <horlet@gmail.com>
2017-07-10 01:16:59 +0300

    basic serializers, views and urls for User and Entry models endpoints

---
+++ mitaba/tracker/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from mitaba.tracker.models import Entry
+from rest_framework import serializers
+
+
+class EntrySerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Entry
+        fields = ('id', 'start', 'stop', 'details', 'owner')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    # entry = serializers.PrimaryKeyRelatedField(many=True, queryset=Entry.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
--- mitaba/tracker/views.py
+++ mitaba/tracker/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from mitaba.tracker.models import Entry
+from mitaba.tracker.serializers import EntrySerializer
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from django.contrib.auth.models import User
+from rest_framework import generics
+from mitaba.tracker.serializers import UserSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'entries': reverse('entry-list', request=request, format=format)
+    })
+
+
+class EntryViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+    #                       IsOwnerOrReadOnly,)
+    queryset = Entry.objects.all()
+    serializer_class = EntrySerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    # permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(username=self.request.user)
