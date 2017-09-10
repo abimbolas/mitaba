9b9738ef1f2c3adc66cba39038f3c66b038f9f1c
slava <horlet@gmail.com>
2017-09-10 22:11:46 +0300

    feature: api-auth url

--- mitaba/urls.py
+++ mitaba/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^oauth/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include('mitaba.tracker.urls')),
 ]
