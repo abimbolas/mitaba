15a7e2f92ef2b9df1909fbea1c527dcd6b661fd7
slava <horlet@gmail.com>
2017-09-23 18:30:22 +0300

    feature: fb demo frontend auth

---
+++ demo/vue-rest-api/src/components/mitaba-auth.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="auth-debug">
+    <h2>Auth API Debug</h2>
+
+    <section>
+      <h3>Facebook</h3>
+      <a :href="fbAuthUrl">Auth</a>
+      <p>{{redirectUri('fb')}}</p>
+      <!--<button href="" @click="connectFacebook()">Connect facebook</button>-->
+      <p>Facebook token: {{tokens.fb}}</p>
+    </section>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        tokens: {
+          fb: ''
+        }
+      }
+    },
+    computed: {
+      fbAuthUrl () {
+        const clientId = '498893767146355'
+        const scope = 'email'
+        return `https://www.facebook.com/v2.10/dialog/oauth?client_id=${clientId}&redirect_uri=${encodeURI(this.redirectUri('fb'))}&scope=${scope}&response_type=code`
+      }
+    },
+    methods: {
+      connectFacebook () {
+        // open url wi
+        this.tokens.fb = 'LOL'
+
+//        this.tokens.fb = url
+//        location.replace(url)
+      },
+
+      redirectUri (type) {
+//        const paths = {
+//          vk: `/#/vk-auth-success`,
+//          fb: `/#/fb-auth-success`
+//        }
+//        return `${location.protocol}//${location.host}${paths[type]}`
+        return `http://local.candyface:8080/success_cb`
+      }
+    }
+
+  }
+</script>
+
+<style>
+
+</style>
--- demo/vue-rest-api/src/router/index.js
+++ demo/vue-rest-api/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Auth from '@/components/mitaba-auth'
 
 Vue.use(Router)
 
@@ -10,6 +11,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: Auth
+    },
+    {
+      path: '/fb-auth-success',
+      name: 'auth-success',
+      component: Auth
     }
   ]
 })
