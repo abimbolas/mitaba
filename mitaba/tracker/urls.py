4e93a751a317b903ccbaa7cf6bb70673cdfd99c8
slava <horlet@gmail.com>
2017-09-24 13:22:14 +0300

    feature: authCodeExchange experimental

--- mitaba/tracker/urls.py
+++ mitaba/tracker/urls.py
@@ -3,9 +3,12 @@ from rest_framework_bulk.routes import BulkRouter
 # from rest_framework.schemas import get_schema_view
 from mitaba.tracker import views
 
-router = BulkRouter(trailing_slash=False)
+router = BulkRouter()
 router.register(r'entries', views.EntryViewSet)
 router.register(r'users', views.UserViewSet)
+# router.register(r'auth-suka', views.AuthCodeExchange, base_name='auth-suka')
+
+authCodeExchange = views.AuthCodeExchange.as_view()
 
 # schema_view = get_schema_view(title='Mitaba API')
 
@@ -13,5 +16,6 @@ router.register(r'users', views.UserViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^auth-suka/$', authCodeExchange, name="auth-suka")
     # url(r'^api/schema/$', schema_view),
 ]
