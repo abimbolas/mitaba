a9946f24381d5686a035103e317e391da6073132
slava <horlet@gmail.com>
2017-10-03 02:59:29 +0300

    fix: user model one to one linked with profile

--- core/models.py
+++ core/models.py
@@ -1,4 +1,3 @@
-import uuid
 from mitaba import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -7,13 +6,16 @@ DEFAULT_OWNER_ID = 1
 
 
 class User(AbstractUser):
-    pass
-    # object_uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    def get_profile(self):
+        try:
+            profile = Profile.objects.get(owner=self)
+        except Profile.DoesNotExist:
+            profile = Profile.objects.create(owner=self)
+        return profile
 
 
 class Profile(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              related_name='profile',
-                              on_delete=models.CASCADE,
-                              default=DEFAULT_OWNER_ID)
+    owner = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                 on_delete=models.CASCADE,
+                                 default=DEFAULT_OWNER_ID)
     picture_url = models.URLField(blank=True)
