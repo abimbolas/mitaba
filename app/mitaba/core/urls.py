33bebcb3ef9ad26bfcb47f5318494b0d9f4e8e92
Виталий Кулешин <antivitla@gmail.com>
2017-10-09 18:48:08 +0300

    Отлючаем админку на продакшне Апдейт README

--- app/mitaba/core/urls.py
+++ app/mitaba/core/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from mitaba.core.settings import DEBUG
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^api/login/', include('rest_social_auth.urls_token')),
     url(r'^api/', include('mitaba.profile.urls')),
     url(r'^api/', include('mitaba.tracker.urls'))
 ]
+
+# Show public admin only in development
+if DEBUG is True:
+  urlpatterns += [url(r'^admin/', admin.site.urls)]
